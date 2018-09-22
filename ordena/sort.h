36b6c790f587a05a2da7db7fa6c6640f586b3cd0
CaioAlarcon <42846850+CaioAlarcon@users.noreply.github.com>
2018-09-22 17:12:33 -0300

    Update sort.h

--- ordena/sort.h
+++ ordena/sort.h
@@ -1,9 +1,11 @@
 //Declara Fun como ponteiro para função void que recebe um vetor int e um int:
 typedef void (*Fun)(int [],int);
 
+//Protótipo das funções:
 void print(int*, int);
-void CriaCenario(char [], int *, int);
-void ajuda(int);//Exibe a mensagem de ajuda
-void init(int [], int, int, int);//Cria os diferentes cenários
+void CriaCenario(char [], int *, int, char []);
+void ajuda(int);
+void init(int [], int, int, int);
 void troca(int *,int *);
 Fun SelAlg(char []);
+void salvar (char [], char [],int, char []);
